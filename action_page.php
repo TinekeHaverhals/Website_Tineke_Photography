951ef77ada8b9cb09e0e1ba916991c155003c47c
TinekeHaverhals <tinekehaverhals@hotmail.com>
2023-03-09 16:44:40 +0100

    Initial commit

---
+++ action_page.php
@@ -0,0 +1,10 @@
+<?php
+
+if($_POST["message"]) {
+
+mail("tinekehaverhals@hotmail.com", "Here is the subject line",
+
+$_POST["insert your message here"]. "From: tinekehaverhals@hotmail.com");
+
+}
+
